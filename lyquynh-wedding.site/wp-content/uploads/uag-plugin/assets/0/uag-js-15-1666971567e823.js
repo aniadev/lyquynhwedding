f6240b90736ba19e5806dab3d707f7281586f118
Hai Pham <haiham1999.90@gmail.com>
2022-11-08 21:31:20 +0700

    init

---
+++ lyquynh-wedding.site/wp-content/uploads/uag-plugin/assets/0/uag-js-15-1666971567e823.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-9742feef' );
+	UAGBTabs.anchorTabId( '.uagb-block-9742feef' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-9742feef' );
+}, false );
+ })
